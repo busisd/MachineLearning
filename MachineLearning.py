9b1e47ab0077c6002372a6545b95252b85a72452
Daniel B <busisd@carleton.edu>
2019-05-18 20:07:32 -0500

    Working, but I'm going to make it look a bit nicer

--- MachineLearning.py
+++ MachineLearning.py
@@ -1,3 +1,5 @@
+import random
+
 class MazeGrid:
 	def __init__(self, rows = 10, cols = 10):
 		self.rows = 10
@@ -40,18 +42,52 @@ def execute_steps(steps, start_pos, grid):
 		pos = step(pos, grid)
 	return pos
 
-def calc_score(steps, pos, target):
+def calc_score(steps, start, target, grid):
+	pos = execute_steps(steps, start, grid)
 	dist_factor = abs(pos[0]-target[0]) + abs(pos[1]-target[1])
 	steps_factor = len(steps)
 	return 1000*dist_factor + steps_factor
+	
+possible_steps = [move_up, move_right, move_left, move_down]
+def perform_mutations(steps, mutations=3, constructiveness=.9):
+	new_steps = steps.copy()
+	for i in range(mutations):
+		r = random.random()
+		if (r<constructiveness):
+			rand_index = random.randrange(len(new_steps)+1)
+			rand_step = random.randrange(len(possible_steps))
+			new_steps.insert(rand_index, possible_steps[rand_step])
+		else:
+			if len(new_steps) > 0:
+				rand_index = random.randrange(len(new_steps))
+				new_steps.pop(rand_index)
+	return new_steps
+
+def find_best_step_list(step_lists, start, target, grid):
+	min_score = calc_score(step_lists[0], start, target, grid)
+	best_list = step_lists[0]
+	for cur_list in step_lists[1:]:
+		cur_list_score = calc_score(cur_list, start, target, grid)
+		if cur_list_score < min_score:
+			min_score = cur_list_score
+			best_list = cur_list
+	return best_list
 
 grid = MazeGrid()
-target = (9,9)
 start = (0,0)
-current_steps = [move_right, move_down, move_right]
-end = execute_steps(current_steps, start, grid)
-print(end)
-print(calc_score(current_steps, end, target))
+target = (9,9)
 
-
+current_steps = []
+for i in range(10):
+	cur_score = calc_score(current_steps, start, target, grid)
+	
+	new_step_lists = []
+	for j in range(5):
+		new_step_lists.append(perform_mutations(current_steps))
+		
+	new_steps = find_best_step_list(new_step_lists, start, target, grid)
+	print(new_steps, end = ' : ')
+	if calc_score(new_steps,start,target,grid)<calc_score(current_steps,start,target,grid):
+		current_steps = new_steps
+	print(current_steps)
 
